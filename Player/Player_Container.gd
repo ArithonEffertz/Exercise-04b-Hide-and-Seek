506fe02128f0e62d124b57b9f1ac833dd4a25c85
ArithonEffertz <70243392+ArithonEffertz@users.noreply.github.com>
2020-10-13 20:56:58 -0400

    Completes assignment

--- Player/Player_Container.gd
+++ Player/Player_Container.gd
@@ -14,3 +14,9 @@ func _physics_process(_delta):
 		player.position = starting_position
 		add_child(player)
 		player.get_node("Camera2D").current = true
+
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		body.die()
